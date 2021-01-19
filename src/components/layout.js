c5813cd8ddbd13538eb0f2836bee2407aec9b71b
Seb Ringrose <sebringrose@gmail.com>
2021-01-19 17:52:55 +0000

    git push

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,13 +1,50 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import Nav from './Nav'
 import styled from 'styled-components'
 import Footer from './Footer'
 import theme from '../styles/theme'
 
+import CookieConsent from "react-cookie-consent"
+
 export default function Layout({children}) {
   const [modal, setModal] = useState(false)
 
   return (<Wrapper>
+    <CookieConsent
+      location="bottom"
+      buttonText="Accept"
+      cookieName="CookieConsent"
+      style={{ background: "#2B373B" }}
+      buttonStyle={{ color: "black", fontSize: "1rem", padding: "0.6rem 1rem" }}
+      enableDeclineButton
+      declineButtonText="Decline"
+      declineButtonStyle={{ background: "none", textDecoration: "underline" }}
+      buttonWrapperClasses="cookie-buttons"
+      onAccept={() => {
+          // Global site tag (gtag.js) - Google Analytics
+          let id = (window.location.href.indexOf('//preview.') !== -1 || window.location.href.indexOf('localhost') !== -1) ? 'UA-116709596-1' : 'UA-116709596-2'
+
+          let gaScript1 = document.createElement("script")
+          gaScript1.async = ""
+          gaScript1.src = `https://www.googletagmanager.com/gtag/js?id='${id}'`
+
+          let gaScript2 = document.createElement('script');
+          gaScript2.text = `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${id}');
+          `;
+            
+
+          document.body.append(gaScript1)
+          document.body.append(gaScript2)
+      }}
+    >
+      <p style={{ fontSize: "1.5rem" }}>Do you accept the use of cookies on this site?</p>
+      <p style={{ fontSize: "1rem" }}>We use cookies for anonymous anayltics purposes only.&nbsp;<a href="https://cognitant.com/privacy">Learn more</a></p>
+    </CookieConsent>
     {modal && <Modal onClick={() => setModal(false)}>
       <ModalContent onClick={(e) => e.stopPropagation()}>{modal}</ModalContent>
     </Modal>}
