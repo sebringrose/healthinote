96bf5871f705072cb563352c2af8fce971204d0a
Seb Ringrose <sebringrose@gmail.com>
2021-02-04 11:50:33 +0000

    end of mo morn 3rd Feb

--- src/sections/HeroSection.js
+++ src/sections/HeroSection.js
@@ -11,12 +11,11 @@ import {
 export default function HeroSection({ content }) {
     return (
         <CenterFlex justify="center" align="center" margin="2rem auto" maxWidth="1500px">
-            {/* Wrapping video in anchor seems to slow down video loading */}
-            {/* <a href={content.linkedVideo} target="_blank"> */}
+            <a href={content.linkedVideo} target="_blank">
                 <HeroVideo preload="true" autoPlay muted loop playsInline>
                     <source src={content.video} type="video/mp4" />
                 </HeroVideo>
-            {/* </a> */}
+            </a>
             <HeroText>
                 <CustomText size={theme.font.xlarge} color={theme.color.blue} weight={600}>{content.heading}</CustomText>
                 <CustomText size={theme.font.large}>{content.subtitle}</CustomText>
@@ -33,21 +32,20 @@ const HeroVideo = styled.video`
     width: 86vw;
     box-sizing: border-box;
     max-width: 400px;
-    margin: 20px 30px;
+    margin: 10px 15px;
     object-fit: contain;
 `
 
 const HeroText = styled.div`
     flex: 1;
-    padding: 20px 30px;
+    padding: 10px 15px;
 `
 
 const HeroBadges = styled.div`
     align-self: flex-end;
     justify-content: space-between;
     width: 160px;
-    margin: 20px 30px;
-    padding: 1rem 1.25rem 1rem;
+    padding: 10px 15px;
     background-color: white;
     text-align: center;
     border-radius: 0.5rem;
